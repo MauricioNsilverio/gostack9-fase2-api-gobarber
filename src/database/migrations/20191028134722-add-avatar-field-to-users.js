d22e7d0294d074c8cdcbb2010a797b3261d2da74
mauricionsilverio <mauricionsilverio@gmail.com>
2019-10-28 11:43:54 -0300

    multer and user avatar setup

---
+++ src/database/migrations/20191028134722-add-avatar-field-to-users.js
@@ -0,0 +1,16 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    // todo avatar_id na tabela usuários vai ser um id na tabela files
+    return queryInterface.addColumn('users', 'avatar_id', {
+      type: Sequelize.INTEGER,
+      references: { model: 'files', key: 'id' },
+      onUpdate: 'CASCADE',
+      onDelete: 'SET NULL',
+      allowNull: true,
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.removeColumn('users', 'avatar_id');
+  },
+};
