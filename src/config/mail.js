791573981cf7058cc8df38f49746f257739b1204
mauricionsilverio <mauricionsilverio@gmail.com>
2019-11-04 17:21:30 -0300

    nodemailer setup

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '76fa639e0acab2',
+    pass: 'c8fd124e5c2f77',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
